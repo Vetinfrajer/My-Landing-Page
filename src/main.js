ab723a441e3858fc65445fd79dfb72c45581c542
vetinfrajer <michalvetiska@email.cz>
2024-09-19 21:31:51 +0200

    mikro chyba v mainjs

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
